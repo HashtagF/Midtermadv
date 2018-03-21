1dedf810356354edb8c6a10abd81906eb0ef4ad1
HashtagF <flamezaxaou1@gmail.com>
2018-03-21 19:31:57 +0700

    Insert Del

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,26 +1,79 @@
 <template>
   <div class="Home container">
-    <h1 class="pb-4">{{msg}}</h1>
+    <img src="../assets/logo.svg" height="160px" width="auto">
     <div class="row justify-content-md-center">
       <div class="col-5">
+        <b-alert variant="success"
+                  :show="dismissCountDown"
+                  dismissible
+                  @dismissed="dismissCountdown=0"
+                  @dismiss-count-down="countDownChanged">
+            Add Todo Success {{dismissCountDown}} seconds...
+          </b-alert>
         <b-form-input type="text"
-                      required
                       placeholder="New Todo"
-                      >
+                      @keyup.native.enter="addTodoAndClearNewTodo"
+                      icon="check"
+                      size="is-large"
+                      v-model="newTodo">
         </b-form-input>
+        <div class="showTodos pt-3 " v-for="(todo, index) in todos" :key="index" >
+          <label class="h4">   {{index + 1}}. {{todo.title}}</label>
+          <button type="button" class="close float-right clearfix" aria-label="Close" @click="removeTodo(todo)">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyCmWtP5PY9Jezs94HjrzvfdxNIAhKcDDXU',
+  authDomain: 'midtermadv.firebaseapp.com',
+  databaseURL: 'https://midtermadv.firebaseio.com',
+  projectId: 'midtermadv',
+  storageBucket: 'midtermadv.appspot.com',
+  messagingSenderId: '498210216713'
+}
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let todosRef = db.ref('todos')
+
 export default {
   name: 'Home',
+  firebase: {
+    todos: todosRef
+  },
   data () {
     return {
-      msg: 'Todo - Firebase',
-      todos: [],
-      newTodo: ''
+      newTodo: '',
+      dismissSecs: 3,
+      dismissCountDown: 0
+    }
+  },
+  methods: {
+    addTodoAndClearNewTodo () {
+      todosRef.push({
+        title: this.newTodo,
+        complete: false
+      })
+      this.newTodo = ''
+      this.showAlert()
+    },
+    removeTodo (todo) {
+      todosRef.child(todo['.key']).remove()
+    },
+    updateTodo (todo, text) {
+      todosRef.child(todo['.key'] + '/text').set(text)
+    },
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert () {
+      this.dismissCountDown = this.dismissSecs
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueFire from 'vuefire'
+
+// explicit installation required in module environments
+Vue.use(VueFire)
 
 Vue.use(BootstrapVue)
 
